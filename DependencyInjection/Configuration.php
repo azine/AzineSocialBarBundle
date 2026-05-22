6fbedb0c12b6ddb1409343e8d896a0b15085e443
azine.me <github@azine-it.ch>
2026-05-22 23:00:17 +0200

    Fix Symfony 7.4 ConfigurationInterface return type

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,7 +15,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('azine_social_bar');
         $rootNode = $treeBuilder->getRootNode();
